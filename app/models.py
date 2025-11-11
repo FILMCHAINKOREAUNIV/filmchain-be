802c1dbd3a1c7a5b011130f441346249536b729e
dxmlk <1101sky@korea.ac.kr>
2025-11-11 15:15:21 +0900

    feat: 해시태그 투표 테이블 추가

--- app/models.py
+++ app/models.py
@@ -19,3 +19,10 @@ class Shorts(Base):
     view_count = Column(BigInteger, default=0)
 
     created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+class HashtagVote(Base):
+    __tablename__ = "hastag_votes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    hashtag = Column(String, unique=True, index=True, nullable=False)
+    vote_count = Column(Integer, default=0)
